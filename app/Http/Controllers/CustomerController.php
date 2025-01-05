eeff1a99203498c3915d51ae1f8493a5174ab6da
Marlo <corridor.marlo@gmail.com>
2025-01-05 10:27:14 +0800

    Add Index page for Customer resource
    - Using resource routing to support other CRUD operations

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CustomerController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Customer/Index', [
+            'customers' => Customer::orderBy('last_name')->get()->map(function ($customer) {
+                return [
+                    'first_name' => $customer->first_name,
+                    'last_name' => $customer->last_name,
+                    'email_address' => $customer->email_address,
+                    'contact_number' => $customer->contact_number,
+                    'show_url' => route('customer.show', $customer),
+                    'edit_url' => route('customer.edit', $customer),
+                ];
+            }),
+            'create_url' => route('customer.create'),
+        ]);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -21,4 +22,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
+    Route::resource('/customer', CustomerController::class);
 });
