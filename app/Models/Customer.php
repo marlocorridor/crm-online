accb46170f2645b72dfc0a00ae3eb770d624bf81
Marlo <corridor.marlo@gmail.com>
2025-01-05 13:02:55 +0800

    Implement soft deletion for Customer resource

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
     use HasFactory;
+    /** @use SoftDeletes */
+    use SoftDeletes;
 
     protected $fillable = [
         'first_name',
