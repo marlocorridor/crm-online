e2d883654dfd6cc3f71a21030894a73c60c2ecaa
Marlo <corridor.marlo@gmail.com>
2025-01-05 10:46:00 +0800

    Cleanup
    Remove unused div

--- resources/js/Pages/Customer/Index.vue
+++ resources/js/Pages/Customer/Index.vue
@@ -17,57 +17,54 @@ defineProps({
     </template>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
-      <div class="">
-        <div class="sm:flex sm:items-center">
-          <div class="sm:flex-auto">
-            <h1 class="text-base font-semibold text-gray-900">Customers</h1>
-            <p class="mt-2 text-sm text-gray-700">A list of all the customers in your account.</p>
-          </div>
-          <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <a :href="create_url" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              New Customer Data
-            </a>
-          </div>
+      <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+          <h1 class="text-base font-semibold text-gray-900">Customers</h1>
+          <p class="mt-2 text-sm text-gray-700">A list of all the customers in your account.</p>
         </div>
-        <div class="mt-8 flow-root">
-          <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-              <div class="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
-                <table class="min-w-full divide-y divide-gray-300">
-                  <thead class="bg-gray-50">
-                    <tr>
-                      <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Last Name</th>
-                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">First Name</th>
-                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
-                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Contact Number</th>
-                      <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                        <span class="sr-only">Edit</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody class="divide-y divide-gray-200 bg-white">
-                    <tr v-for="person in customers" :key="person.email">
-                      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ person.last_name }}</td>
-                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.first_name }}</td>
-                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email_address }}</td>
-                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.contact_number }}</td>
-                      <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <Link :href="person.show_url" class="text-indigo-600 hover:text-indigo-900 ml-2">
-                          View<span class="sr-only">, {{ person.name }}</span>
-                        </Link>
-                        <Link :href="person.edit_url" class="text-indigo-600 hover:text-indigo-900 ml-2">
-                          Edit<span class="sr-only">, {{ person.name }}</span>
-                        </Link>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <a :href="create_url" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            New Customer Data
+          </a>
+        </div>
+      </div>
+      <div class="mt-8 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <div class="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
+              <table class="min-w-full divide-y divide-gray-300">
+                <thead class="bg-gray-50">
+                  <tr>
+                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Last Name</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">First Name</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Contact Number</th>
+                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                  <tr v-for="person in customers" :key="person.email">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ person.last_name }}</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.first_name }}</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email_address }}</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.contact_number }}</td>
+                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                      <Link :href="person.show_url" class="text-indigo-600 hover:text-indigo-900 ml-2">
+                        View<span class="sr-only">, {{ person.name }}</span>
+                      </Link>
+                      <Link :href="person.edit_url" class="text-indigo-600 hover:text-indigo-900 ml-2">
+                        Edit<span class="sr-only">, {{ person.name }}</span>
+                      </Link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
       </div>
-
     </div>
 
   
