39dff4939c2594d896ac618562710cfb39d7a665
Marlo <corridor.marlo@gmail.com>
2025-01-05 13:22:33 +0800

    Align action buttons to right
    To support left-to-right ease of use

--- resources/js/Pages/Customer/Edit.vue
+++ resources/js/Pages/Customer/Edit.vue
@@ -82,11 +82,13 @@ const form = useForm({
                     </div>
                   </div>
                 </div>
-                <button type="submit" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  :disabled="form.processing"
-                >
-                  Submit
-                </button>
+                <div class="mt-2 flex flex-row-reverse">
+                  <button type="submit" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    :disabled="form.processing"
+                  >
+                    Update
+                  </button>
+                </div>
               </form>
           </div>
         </div>
